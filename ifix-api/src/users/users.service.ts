03e5fb3decb5d1da30d531c935f805956057acb6
quevedogl <engs-guilhermequevedo@camporeal.edu.br>
2026-05-09 20:59:47 -0300

    D4 API BANCO DE DADOS

---
+++ ifix-api/src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+}
